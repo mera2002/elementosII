c7a72917a95f2487c6b1fd4d860b0ae83f087c84
mera2002 <SMIS072121@ugb.edu.sv>
2021-08-12 19:45:32 -0600

    Nota mayor, Nombres de estudiantes.

--- Semana3Practica/Arreglos.cs
+++ Semana3Practica/Arreglos.cs
@@ -11,47 +11,84 @@ namespace Semana3Practica
         static void Main()
         {
             Double promedio;
-           
+            int mayor = 0, j = 0;
             int suma = 0;
-            
-           
-        //calcular la nota promedio de N notas 
-        int[] notas = new int[10];
+
+            string[] nombres = new string[3];
+            for (int i = 0; i < nombres.Length; i++)
+
+            {
+                Console.WriteLine("Ingrese el nombre del estudiante {0}:  ", i);
+                nombres[i] = Console.ReadLine();
+                
+
+
+            }
+
+
+            //calcular la nota promedio de N notas 
+            int[] notas = new int[3];
 
             //capturar las notas 
             for (int i = 0; i < notas.Length; i++)
             {
                 //solicitar las notas al usuario 
-                Console.Write("Escriba la nota {0}:", i);
+                Console.Write("Ingrese la nota {0}:  ", i);
                 notas[i] = Convert.ToInt32(Console.ReadLine());
             }
 
             //recorrer el arreglo de notas 
-            for (int i = 0; i < notas.Length; i++)
-            {
-                Console.WriteLine("Nota {0}: {1}", i, notas[i]); //Imprime los valores 
+            
 
-               
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine(" Nombre del estudiante {0}: {1} ", i, nombres[i]); //Imprime los valores 
 
 
             }
+
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine(" Nota del estudiante {0}: {1} ", i, notas[i]); //Imprime los valores 
+
+
+            }
+
+
+
             for (int i = 0; i < notas.Length; i++)
 
                 suma = suma + notas[i]; 
                 promedio = suma / notas.Length;
             {
 
+         
                 Console.WriteLine("---------------------------------------------");
-                Console.WriteLine("La suma de las notas es: "+suma);
-                Console.WriteLine("---------------------------------------------");
-                Console.WriteLine("El promedio de notas es notas es: " + promedio);
+                Console.WriteLine("El promedio de notas es : " + promedio);
+
+
+               
+                    
             }
 
-           
-            
+            while (j < 3)
+         
+
+            {
+                if (notas[j] > mayor)
+                    mayor = notas[j];
+                j++; 
+                    
+
+                }
+            Console.WriteLine("La nota mayor es: {0}", mayor);
             Console.ReadKey();
         }
+    }
         
 
-    }
+    
 }
